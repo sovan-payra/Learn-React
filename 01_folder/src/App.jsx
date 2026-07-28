eacad2e00228518b028baebd6db8f7e76e46226c
sovan-payra <sovanpayra019@gmail.com>
2026-07-28 22:29:24 +0530

    01_folder done

---
+++ 01_folder/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="parent">I am Parent</div>
+      <h1>Hello I am h1</h1>
+      <div className="child">I am Child</div>
+    </>
+  );
+};
+
+export default App;
