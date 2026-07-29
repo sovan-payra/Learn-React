64b74fbb0e2d58f5d0cb019ab37ddc2b96abe909
sovan-payra <sovanpayra019@gmail.com>
2026-07-29 09:27:14 +0530

    Add 02_components

---
+++ 02_components/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+        <h1>Sovan Payra</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, quibusdam. Nemo corrupti rerum unde magnam quos, deserunt incidunt dignissimos, doloribus omnis soluta iusto facere.</p>
+    </div>
+  )
+}
+
+export default Card
