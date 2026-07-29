24672fe7c89fb6b0330a98999aad7ca91976dc44
Sovan Payra <120330262+sovan-payra@users.noreply.github.com>
2026-07-29 10:36:13 +0530

    Update Card.jsx

--- 03_props/src/components/Card.jsx
+++ 03_props/src/components/Card.jsx
@@ -5,7 +5,6 @@ const Card = (props) => {
     <div className='card'>
         <img src={props.img} alt="" />
         <h1>{props.user}, {props.age}</h1>
-
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quia modi fugit, placeat nihil dignissimos nemo. Repellendus impedit, optio molestiae officia ipsam quaerat.</p>
         <button>View Profile</button>
     </div>
